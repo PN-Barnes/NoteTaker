000459ad66f3e9ff419ecdca6407c030a792271e
PN-Barnes <paulieb99@gmail.com>
2021-07-21 21:15:23 -0700

    updated middleware

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(PORT, () =>
   console.log(`listening at http://localhost:${PORT}`)
