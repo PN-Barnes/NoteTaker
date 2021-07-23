50178b45d9c744bf38ad465b9fbd773fb836bb4a
PN-Barnes <paulieb99@gmail.com>
2021-07-22 17:45:58 -0700

    added fucntion to eventlistener call to create note on front end before fetch post

--- Develop/public/scripts/notes.js
+++ Develop/public/scripts/notes.js
@@ -2,7 +2,19 @@ const noteTitle = document.getElementById('noteTitle')
 const noteText = document.getElementById('noteText')
 const addButton = document.getElementById('addButton')
 
+const addNote = event => {
+    event.preventDefault();
+    console.log('Add Note submitted');
+    
+    const noteName = noteTitle.value;
+    const note = noteText.value;
 
+    const newNote = {
+        noteTitle: noteName,
+        note: note
+    
+    }
+}
 
 
 addButton.addEventListener('click', addNote)
