4ee4869f6b0430e1d2439ad7d4bc6b8d59c8ef6d
PN-Barnes <paulieb99@gmail.com>
2021-07-22 17:14:09 -0700

    added routes and scripts folder and connected api through modularization.

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
-
+const api = require('./routes/index.js')
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () =>
   console.log(`listening at http://localhost:${PORT}`)
