9f81621e1768b27b7e10c1cf42520e4a3b7bb29d
PN-Barnes <paulieb99@gmail.com>
2021-07-22 17:35:48 -0700

    added pointers to elements on notes.html and added event listener

---
+++ Develop/public/scripts/notes.js
@@ -0,0 +1,8 @@
+const noteTitle = document.getElementById('noteTitle')
+const noteText = document.getElementById('noteText')
+const addButton = document.getElementById('addButton')
+
+
+
+
+addButton.addEventListener('click', addNote)
--- Develop/server.js
+++ Develop/server.js
@@ -6,11 +6,16 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
+
 app.use(express.static('public'));
 
+// GET route for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+// GET route for Notes Page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
