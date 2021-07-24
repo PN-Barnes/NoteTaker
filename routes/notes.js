b3231dbfe28ca46dfed29f433c034d3a46fdfef2
PN-Barnes <paulieb99@gmail.com>
2021-07-23 22:32:57 -0700

    Debugging of notes page

--- routes/notes.js
+++ routes/notes.js
@@ -2,10 +2,11 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../public/helpers/fsUtils');
 const uuid = require('../public/helpers/uuid');
 
+
 // GET ROUTE FOR RETRIEVING ALL NOTES
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 // POST ROUTE for new notes
@@ -13,7 +14,7 @@ notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
     console.log(req.body)
 
-    const { noteTitle, note } = req.body;
+    const { title, text } = req.body;
   
     if (req.body) {
       const newNote = {
@@ -22,7 +23,7 @@ notes.post('/', (req, res) => {
         Id: uuid()
       };
 
-      readAndAppend(newNote, './db/db.json');
+      readAndAppend(newNote, '../db/db.json');
       res.json(`Note Added Successfully 🚀`)
     }  else {
         res.error('Error in handling note')
