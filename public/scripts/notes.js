38bcb0ba337fc3f39538ecd7d4003a426d46e47c
PN-Barnes <paulieb99@gmail.com>
2021-07-23 19:05:18 -0700

    added getNotes function to retrieve notes already within the server

--- public/scripts/notes.js
+++ public/scripts/notes.js
@@ -2,11 +2,23 @@ const noteTitle = document.getElementById('noteTitle')
 const noteText = document.getElementById('noteText')
 const addButton = document.getElementById('addButton')
 
+// GET THE NOTES THAT EXIST WITHIN THE SERVER
+const getNotes = () => 
+  fetch('http://localhost:3001/api/notes', {
+    method: 'GET',
+    header: {
+      'Content-Type': 'application/json'
+    },
 
-
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => {
+      console.error('Error:', error)
+    })
 
 const postNote = (tip) =>
-fetch('http://localhost:3001/api/tips', {
+fetch('http://localhost:3001/api/notes', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
