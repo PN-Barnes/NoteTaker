6ad5459444b2a6a967106ecc3ee441bca66b7dbc
PN-Barnes <paulieb99@gmail.com>
2021-07-23 23:06:07 -0700

    third stage of debugging

--- routes/notes.js
+++ routes/notes.js
@@ -6,24 +6,23 @@ const uuid = require('../public/helpers/uuid');
 // GET ROUTE FOR RETRIEVING ALL NOTES
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 // POST ROUTE for new notes
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
     console.log(req.body)
-
     const { title, text } = req.body;
   
     if (req.body) {
       const newNote = {
         title,
         text,
-        Id: uuid()
+        id: uuid()
       };
 
-      readAndAppend(newNote, '../db/db.json');
+      readAndAppend(newNote, './db/db.json');
       res.json(`Note Added Successfully 🚀`)
     }  else {
         res.error('Error in handling note')
